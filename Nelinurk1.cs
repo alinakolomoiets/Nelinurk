6dd11036b90aa299e245877186e7601bd8866fe2
opilane <opilane@HP-CZC6297BB1>
2022-03-02 11:00:29 +0200

    Add project files.

---
+++ Nelinurk1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nelinurk
+{
+	public class Nelinurk1
+	{
+		int kõrgus;
+		int laius;
+		string ümbermõõt;
+
+		public Nelinurk1() { }//пустой конструктор
+		public Nelinurk1(int kõrgus, int laius)
+		{
+			kõrgus = Kõrgus;
+			laius = Laius;
+			ümbermõõt = Ümbermõõt;
+		}
+		public int Kõrgus {
+			set { kõrgus = value; }
+			get { return kõrgus; }
+		}
+		public int Laius {
+			set { laius = value; }
+			get { return laius; }
+		}
+		public int Ümbermõõt { 	
+			set { ümbermõõt = value; }
+			get { return ümbermõõt; }
+		}
+		public double Ümber()
+		{
+			double ümber = 0;
+			ümber = (kõrgus+laius)*2;
+			return ümber ;
+		}
+			public void Info ()
+		{ 
+			Console.WriteLine("Tere");
+			Console.WriteLine("Minu kõrgus on {0} . Minu laius on {1} . Minu ", kõrgus, laius);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nelinurk
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Nelinurk1 kõr = new Nelinurk1();
+			kõr = new Nelinurk1();
+			Console.WriteLine("Kõrgus:");
+			kõr.Kõrgus = int.Parse(Console.ReadLine());
+			Console.WriteLine("Laius:");
+			kõr.Laius = int.Parse(Console.ReadLine());
+			kõr.Info();
+
+			Console.ReadLine();
+
+		}
+
+
+	}
+}
