a8f6f47b5365c1ffbaca3e05ea1872df53aa062f
Alina <93324365+alinakolomoiets@users.noreply.github.com>
2022-03-07 10:12:34 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,18 +11,16 @@ namespace Nelinurk
 		static void Main(string[] args)
 		{
 			Nelinurk1 kõr = new Nelinurk1();
-			kõr = new Nelinurk1();
 			Console.WriteLine("Kõrgus:");
 			kõr.Kõrgus = int.Parse(Console.ReadLine());
 			Console.WriteLine("Laius:");
 			kõr.Laius = int.Parse(Console.ReadLine());
 			double ümber = kõr.Ümber();
+			double pindala = kõr.Pindala();
 			kõr.Info();
+			Console.WriteLine("Minu pindla on {0} . Minu  ümber {1}", pindala, ümber);
 
 			Console.ReadLine();
-
 		}
-
-
 	}
-}
+}	
